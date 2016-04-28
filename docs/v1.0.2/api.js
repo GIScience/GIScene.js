2055f2028429b3465cf5174196ece392af01c90c
mcauer <auer@uni-heidelberg.de>
2016-04-28 17:30:09 +0200

    v1.0.2

---
+++ docs/v1.0.2/api.js
@@ -0,0 +1,73 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "GIScene",
+        "GIScene.AspectMaterial",
+        "GIScene.Control",
+        "GIScene.Control.AxisHelper",
+        "GIScene.Control.CameraLight",
+        "GIScene.Control.Compass",
+        "GIScene.Control.EdgeDetectionFreiChen",
+        "GIScene.Control.Fly",
+        "GIScene.Control.LoadIndicator",
+        "GIScene.Control.Measure",
+        "GIScene.Control.ObjectPosition",
+        "GIScene.Control.OrbitZoomPan",
+        "GIScene.Control.Pan",
+        "GIScene.Control.PanOrbitZoomCenter",
+        "GIScene.Control.Pick",
+        "GIScene.Control.SSAO",
+        "GIScene.Control.Select",
+        "GIScene.Control.TextPanel",
+        "GIScene.Control.Trackball",
+        "GIScene.Control.Walk",
+        "GIScene.DirectionMaterial",
+        "GIScene.DistanceOpacityMaterial",
+        "GIScene.Extent2",
+        "GIScene.Format",
+        "GIScene.GeoJSONLoader",
+        "GIScene.Grid",
+        "GIScene.Grid.Index",
+        "GIScene.Layer",
+        "GIScene.Layer.Fixed",
+        "GIScene.Layer.Grid",
+        "GIScene.Layer.Helper",
+        "GIScene.Layer.W3DS_0_4_0",
+        "GIScene.Layer.W3DS_0_4_1",
+        "GIScene.Line2",
+        "GIScene.LocalFileLoader",
+        "GIScene.ModelLoader",
+        "GIScene.OverrideMaterialHandler",
+        "GIScene.OverrideMaterialHandler.WMS",
+        "GIScene.PointAlignmentMaterial",
+        "GIScene.Process",
+        "GIScene.Process.Intervisibility",
+        "GIScene.Process.LineOfSight",
+        "GIScene.Process.LineOfSightNetwork_allToAll",
+        "GIScene.Process.LineOfSight_Layer",
+        "GIScene.Process.LineOfSight_fastClient",
+        "GIScene.Process.LineOfSight_simpleClient",
+        "GIScene.RasterOverlayMaterial",
+        "GIScene.Scene",
+        "GIScene.Service",
+        "GIScene.Service.W3DS_0_4_0",
+        "GIScene.Service.W3DS_0_4_1",
+        "GIScene.Style",
+        "GIScene.Utils",
+        "GIScene.WMSOverlayMaterial",
+        "GIscene.Coordinate2",
+        "GIscene.Coordinate3"
+    ],
+    "modules": [
+        "GIScene"
+    ],
+    "allModules": [
+        {
+            "displayName": "GIScene",
+            "name": "GIScene",
+            "description": "Base Object wich gives the namespace for all other classes of the library.\nDefines global constants used in other classes"
+        }
+    ],
+    "elements": []
+} };
+});
