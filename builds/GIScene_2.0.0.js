0b24ee15aad63b930c111b6b309dd0c373ec4160
Michael Auer <michael.auer@geog.uni-heidelberg.de>
2016-05-15 01:54:51 +0200

    updated builds

--- builds/GIScene_2.0.0.js
+++ builds/GIScene_2.0.0.js
@@ -1574,6 +1574,8 @@ THREE.SceneLoader = function ( manager ) {
 
 	this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
 
+	//added by mca
+	this.loader = new THREE.XHRLoader( this.manager );
 };
 
 THREE.SceneLoader.prototype = {
@@ -1584,12 +1586,15 @@ THREE.SceneLoader.prototype = {
 
 		var scope = this;
 
-		var loader = new THREE.XHRLoader( scope.manager );
-		loader.load( url, function ( text ) {
+		//var loader = new THREE.XHRLoader( scope.manager ); //mca
+		var loader = this.loader;
+		var request = loader.load( url, function ( text ) {
 
 			scope.parse( JSON.parse( text ), onLoad, url );
 
 		}, onProgress, onError );
+		
+		loader.request = request;
 
 	},
 
@@ -7083,27 +7088,7 @@ GIScene.RasterOverlayMaterial = function(config) {
 						"diffuse"    : { type: "c", value: new THREE.Color(this.color) },
 					  }
 				]),
-				// uniforms: THREE.UniformsUtils.merge( [
-// 		
-					// THREE.UniformsLib[ "common" ],
-					// THREE.UniformsLib[ "fog" ],
-					// THREE.UniformsLib[ "lights" ],
-					// THREE.UniformsLib[ "shadowmap" ],
-// 		
-					// {
-						// "diffuse" : { type: "c", value: new THREE.Color( 0x36421E ) },
-						// "ambient"  : { type: "c", value: new THREE.Color( 0x36421E ) },
-						// "emissive" : { type: "c", value: this.emissive/*new THREE.Color( 0x000000 ) */},
-						// "wrapRGB"  : { type: "v3", value: new THREE.Vector3( 1, 1, 1 ) },
-// 					
-						// // "mNear": { type: "f", value: near },
-						// // "mFar" : { type: "f", value: far }
-						// "tOverlay"   : { type: "t",  value : this.texture},
-						// "lowerLeft"  : { type: "v2", value : this.lowerLeft},
-						// "upperRight" : { type: "v2", value : this.upperRight}
-					// }
-// 		
-				// ] ),
+				
 		
 				vertexShader: [
 				
@@ -7165,261 +7150,7 @@ GIScene.RasterOverlayMaterial = function(config) {
 					"}"
 					
 				].join("\n"),
-				vertexShader_r63: [
-					"varying vec3 vWorldPosition;",
-		
-					"#define LAMBERT",
-		
-					"varying vec3 vLightFront;",
-					
-					"varying vec3 vLightingOnly;",//mca
-		
-					"#ifdef DOUBLE_SIDED",
-		
-						"varying vec3 vLightBack;",
-		
-					"#endif",
-		
-					THREE.ShaderChunk[ "map_pars_vertex" ],
-					THREE.ShaderChunk[ "lightmap_pars_vertex" ],
-					THREE.ShaderChunk[ "envmap_pars_vertex" ],
-					THREE.ShaderChunk[ "lights_lambert_pars_vertex" ],
-					THREE.ShaderChunk[ "color_pars_vertex" ],
-					THREE.ShaderChunk[ "morphtarget_pars_vertex" ],
-					THREE.ShaderChunk[ "skinning_pars_vertex" ],
-					THREE.ShaderChunk[ "shadowmap_pars_vertex" ],
-		
-					"void main() {",
-		
-						THREE.ShaderChunk[ "map_vertex" ],
-						THREE.ShaderChunk[ "lightmap_vertex" ],
-						THREE.ShaderChunk[ "color_vertex" ],
-		
-						THREE.ShaderChunk[ "morphnormal_vertex" ],
-						THREE.ShaderChunk[ "skinbase_vertex" ],
-						THREE.ShaderChunk[ "skinnormal_vertex" ],
-						THREE.ShaderChunk[ "defaultnormal_vertex" ],
-		
-						THREE.ShaderChunk[ "morphtarget_vertex" ],
-						THREE.ShaderChunk[ "skinning_vertex" ],
-						THREE.ShaderChunk[ "default_vertex" ],
-		
-						THREE.ShaderChunk[ "worldpos_vertex" ],
-						THREE.ShaderChunk[ "envmap_vertex" ],
-						//mca THREE.ShaderChunk[ "lights_lambert_vertex" ],
-								"vLightFront = vec3( 0.0 );",
-
-								"#ifdef DOUBLE_SIDED",
-						
-									"vLightBack = vec3( 0.0 );",
-						
-								"#endif",
-						
-								"transformedNormal = normalize( transformedNormal );",
-						
-								"#if MAX_DIR_LIGHTS > 0",
-						
-								"for( int i = 0; i < MAX_DIR_LIGHTS; i ++ ) {",
-						
-									"vec4 lDirection = viewMatrix * vec4( directionalLightDirection[ i ], 0.0 );",
-									"vec3 dirVector = normalize( lDirection.xyz );",
-						
-									"float dotProduct = dot( transformedNormal, dirVector );",
-									"vec3 directionalLightWeighting = vec3( max( dotProduct, 0.0 ) );",
-						
-									"#ifdef DOUBLE_SIDED",
-						
-										"vec3 directionalLightWeightingBack = vec3( max( -dotProduct, 0.0 ) );",
-						
-										"#ifdef WRAP_AROUND",
-						
-											"vec3 directionalLightWeightingHalfBack = vec3( max( -0.5 * dotProduct + 0.5, 0.0 ) );",
-						
-										"#endif",
-						
-									"#endif",
-						
-									"#ifdef WRAP_AROUND",
-						
-										"vec3 directionalLightWeightingHalf = vec3( max( 0.5 * dotProduct + 0.5, 0.0 ) );",
-										"directionalLightWeighting = mix( directionalLightWeighting, directionalLightWeightingHalf, wrapRGB );",
-						
-										"#ifdef DOUBLE_SIDED",
-						
-											"directionalLightWeightingBack = mix( directionalLightWeightingBack, directionalLightWeightingHalfBack, wrapRGB );",
-						
-										"#endif",
-						
-									"#endif",
-						
-									"vLightFront += directionalLightColor[ i ] * directionalLightWeighting;",
-						
-									"#ifdef DOUBLE_SIDED",
-						
-										"vLightBack += directionalLightColor[ i ] * directionalLightWeightingBack;",
-						
-									"#endif",
-						
-								"}",
-						
-								"#endif",
-						
-								"#if MAX_POINT_LIGHTS > 0",
-						
-									"for( int i = 0; i < MAX_POINT_LIGHTS; i ++ ) {",
-						
-										"vec4 lPosition = viewMatrix * vec4( pointLightPosition[ i ], 1.0 );",
-										"vec3 lVector = lPosition.xyz - mvPosition.xyz;",
-						
-										"float lDistance = 1.0;",
-										"if ( pointLightDistance[ i ] > 0.0 )",
-											"lDistance = 1.0 - min( ( length( lVector ) / pointLightDistance[ i ] ), 1.0 );",
-						
-										"lVector = normalize( lVector );",
-										"float dotProduct = dot( transformedNormal, lVector );",
-						
-										"vec3 pointLightWeighting = vec3( max( dotProduct, 0.0 ) );",
-						
-										"#ifdef DOUBLE_SIDED",
-						
-											"vec3 pointLightWeightingBack = vec3( max( -dotProduct, 0.0 ) );",
-						
-											"#ifdef WRAP_AROUND",
-						
-												"vec3 pointLightWeightingHalfBack = vec3( max( -0.5 * dotProduct + 0.5, 0.0 ) );",
-						
-											"#endif",
-						
-										"#endif",
-						
-										"#ifdef WRAP_AROUND",
-						
-											"vec3 pointLightWeightingHalf = vec3( max( 0.5 * dotProduct + 0.5, 0.0 ) );",
-											"pointLightWeighting = mix( pointLightWeighting, pointLightWeightingHalf, wrapRGB );",
-						
-											"#ifdef DOUBLE_SIDED",
-						
-												"pointLightWeightingBack = mix( pointLightWeightingBack, pointLightWeightingHalfBack, wrapRGB );",
-						
-											"#endif",
-						
-										"#endif",
-						
-										"vLightFront += pointLightColor[ i ] * pointLightWeighting * lDistance;",
-						
-										"#ifdef DOUBLE_SIDED",
-						
-											"vLightBack += pointLightColor[ i ] * pointLightWeightingBack * lDistance;",
-						
-										"#endif",
-						
-									"}",
-						
-								"#endif",
-						
-								"#if MAX_SPOT_LIGHTS > 0",
-						
-									"for( int i = 0; i < MAX_SPOT_LIGHTS; i ++ ) {",
-						
-										"vec4 lPosition = viewMatrix * vec4( spotLightPosition[ i ], 1.0 );",
-										"vec3 lVector = lPosition.xyz - mvPosition.xyz;",
-						
-										"float spotEffect = dot( spotLightDirection[ i ], normalize( spotLightPosition[ i ] - worldPosition.xyz ) );",
-						
-										"if ( spotEffect > spotLightAngleCos[ i ] ) {",
-						
-											"spotEffect = max( pow( spotEffect, spotLightExponent[ i ] ), 0.0 );",
-						
-											"float lDistance = 1.0;",
-											"if ( spotLightDistance[ i ] > 0.0 )",
-												"lDistance = 1.0 - min( ( length( lVector ) / spotLightDistance[ i ] ), 1.0 );",
-						
-											"lVector = normalize( lVector );",
-						
-											"float dotProduct = dot( transformedNormal, lVector );",
-											"vec3 spotLightWeighting = vec3( max( dotProduct, 0.0 ) );",
-						
-											"#ifdef DOUBLE_SIDED",
-						
-												"vec3 spotLightWeightingBack = vec3( max( -dotProduct, 0.0 ) );",
-						
-												"#ifdef WRAP_AROUND",
-						
-													"vec3 spotLightWeightingHalfBack = vec3( max( -0.5 * dotProduct + 0.5, 0.0 ) );",
-						
-												"#endif",
-						
-											"#endif",
-						
-											"#ifdef WRAP_AROUND",
-						
-												"vec3 spotLightWeightingHalf = vec3( max( 0.5 * dotProduct + 0.5, 0.0 ) );",
-												"spotLightWeighting = mix( spotLightWeighting, spotLightWeightingHalf, wrapRGB );",
-						
-												"#ifdef DOUBLE_SIDED",
-						
-													"spotLightWeightingBack = mix( spotLightWeightingBack, spotLightWeightingHalfBack, wrapRGB );",
-						
-												"#endif",
-						
-											"#endif",
-						
-											"vLightFront += spotLightColor[ i ] * spotLightWeighting * lDistance * spotEffect;",
-						
-											"#ifdef DOUBLE_SIDED",
-						
-												"vLightBack += spotLightColor[ i ] * spotLightWeightingBack * lDistance * spotEffect;",
-						
-											"#endif",
-						
-										"}",
-						
-									"}",
-						
-								"#endif",
-						
-								"#if MAX_HEMI_LIGHTS > 0",
-						
-									"for( int i = 0; i < MAX_HEMI_LIGHTS; i ++ ) {",
-						
-										"vec4 lDirection = viewMatrix * vec4( hemisphereLightDirection[ i ], 0.0 );",
-										"vec3 lVector = normalize( lDirection.xyz );",
-						
-										"float dotProduct = dot( transformedNormal, lVector );",
-						
-										"float hemiDiffuseWeight = 0.5 * dotProduct + 0.5;",
-										"float hemiDiffuseWeightBack = -0.5 * dotProduct + 0.5;",
-						
-										"vLightFront += mix( hemisphereLightGroundColor[ i ], hemisphereLightSkyColor[ i ], hemiDiffuseWeight );",
-						
-										"#ifdef DOUBLE_SIDED",
-						
-											"vLightBack += mix( hemisphereLightGroundColor[ i ], hemisphereLightSkyColor[ i ], hemiDiffuseWeightBack );",
-						
-										"#endif",
-						
-									"}",
-						
-								"#endif",
-								
-								"vLightingOnly = vLightFront;", //mca
-						
-								"vLightFront = vLightFront * diffuse + ambient * ambientLightColor + emissive;",
-						
-								"#ifdef DOUBLE_SIDED",
-						
-									"vLightBack = vLightBack * diffuse + ambient * ambientLightColor + emissive;",
-						
-								"#endif",
-								
-								
-						THREE.ShaderChunk[ "shadowmap_vertex" ],
-						
-						
-						"vWorldPosition = worldPosition.xyz;",
-					"}"
-		
-				].join("\n"),
+				
 				
 				fragmentShader: [
 					//mca
@@ -7550,124 +7281,9 @@ GIScene.RasterOverlayMaterial = function(config) {
 					 
 					"}"
 					
-				].join("\n"),
+				].join("\n")
 		
-				fragmentShader_r63: [
-					//mca
-					// "uniform float mNear;",
-					// "uniform float mFar;",
-					"varying vec3 vWorldPosition;",
-					"uniform vec2 lowerLeft;",
-					"uniform vec2 upperRight;",
-					"uniform sampler2D tOverlay;",
 					
-					"uniform vec3 ambient;",
-					"uniform vec3 diffuse;",
-					"uniform vec3 emissive;",
-					"uniform vec3 ambientLightColor;",
-					
-					"uniform float opacity;",
-		
-					"varying vec3 vLightFront;",
-					
-					"varying vec3 vLightingOnly;",//mca
-		
-					"#ifdef DOUBLE_SIDED",
-		
-						"varying vec3 vLightBack;",
-		
-					"#endif",
-		
-					THREE.ShaderChunk[ "color_pars_fragment" ],
-					THREE.ShaderChunk[ "map_pars_fragment" ],
-					THREE.ShaderChunk[ "lightmap_pars_fragment" ],
-					THREE.ShaderChunk[ "envmap_pars_fragment" ],
-					THREE.ShaderChunk[ "fog_pars_fragment" ],
-					THREE.ShaderChunk[ "shadowmap_pars_fragment" ],
-					THREE.ShaderChunk[ "specularmap_pars_fragment" ],
-		
-					"void main() {",
-					
-						
-						
-						
-						"gl_FragColor = vec4( vec3 ( 1.0 ), opacity );",
-						
-						
-		
-						THREE.ShaderChunk[ "map_fragment" ],
-						// "if(vWorldPosition.x >= lowerLeft.x && vWorldPosition.x <= upperRight.x && -vWorldPosition.z >= lowerLeft.y && -vWorldPosition.z <= upperRight.y)",
-							// "{",
-								// "vec2 worldPosGeo = vec2(vWorldPosition.x, -vWorldPosition.z);",
-								// "vec4 tColor = texture2D(tOverlay,  (worldPosGeo-lowerLeft) / (upperRight - lowerLeft));",
-								// // use emissive for selection, don't subtract it  "gl_FragColor = mix(gl_FragColor,vec4(((gl_FragColor.rgb - emissive - (ambient * ambientLightColor) )/diffuse) * tColor.rgb, opacity ),tColor.a) ;",
-								// "gl_FragColor = mix(gl_FragColor,vec4((((gl_FragColor.rgb - emissive - (ambient * ambientLightColor) )/diffuse) +emissive)* tColor.rgb, opacity ),tColor.a) ;",
-// 								
-								// //new test
-// 								
-								// // "gl_FragColor =  mix(vec4(1.0),tColor, tColor.a);",
-// 								
-							// "}",
-						THREE.ShaderChunk[ "alphatest_fragment" ],
-						THREE.ShaderChunk[ "specularmap_fragment" ],
-		
-						"#ifdef DOUBLE_SIDED",
-		
-							//"float isFront = float( gl_FrontFacing );",
-							//"gl_FragColor.xyz *= isFront * vLightFront + ( 1.0 - isFront ) * vLightBack;",
-		
-							"if ( gl_FrontFacing )",
-								"gl_FragColor.xyz *= vLightFront;",
-							"else",
-								"gl_FragColor.xyz *= vLightBack;",
-		
-						"#else",
-		
-							"gl_FragColor.xyz *= vLightFront;",
-		
-						"#endif",
-		
-						THREE.ShaderChunk[ "lightmap_fragment" ],
-						//this was ShaderChunk color_fragment
-						"#ifdef USE_COLOR",
-							
-							"gl_FragColor = gl_FragColor * vec4( vColor, opacity );",
-
-						"#endif",
-						THREE.ShaderChunk[ "envmap_fragment" ],
-						THREE.ShaderChunk[ "shadowmap_fragment" ],
-		
-						THREE.ShaderChunk[ "linear_to_gamma_fragment" ],
-		
-						THREE.ShaderChunk[ "fog_fragment" ],
-						
-						//mca
-						// "float depth_ = gl_FragCoord.z / gl_FragCoord.w;",
-						// "float distOpacity = smoothstep( mNear, mFar, depth_ );",
-						// "gl_FragColor = vec4( gl_FragColor.rgb, distOpacity );",
-						// "if (distOpacity < 0.01) discard;", //depthBuffer will not be written
-						
-						"if(vWorldPosition.x >= lowerLeft.x && vWorldPosition.x <= upperRight.x && -vWorldPosition.z >= lowerLeft.y && -vWorldPosition.z <= upperRight.y)",
-							"{",
-								"vec2 worldPosGeo = vec2(vWorldPosition.x, -vWorldPosition.z);",
-								"vec4 tColor = texture2D(tOverlay,  (worldPosGeo-lowerLeft) / (upperRight - lowerLeft));",
-								// use emissive for selection, don't subtract it  "gl_FragColor = mix(gl_FragColor,vec4(((gl_FragColor.rgb - emissive - (ambient * ambientLightColor) )/diffuse) * tColor.rgb, opacity ),tColor.a) ;",
-								//"gl_FragColor = mix(gl_FragColor,vec4(((gl_FragColor.rgb - (ambient * ambientLightColor) )/diffuse) * tColor.rgb, opacity ),tColor.a) ;",
-								
-								//new test
-								
-								//"gl_FragColor =  mix(vec4(vec3(1.0),opacity),vec4(tColor.rgb* (((vLightFront-emissive-(ambient * ambientLightColor))/diffuse)+emissive), opacity), tColor.a);",
-								//"gl_FragColor =  mix(gl_FragColor,vec4(tColor.rgb* (((vLightFront-emissive-(ambient * ambientLightColor))/diffuse)+emissive+ambientLightColor), opacity), tColor.a);",
-								//"gl_FragColor =  mix(gl_FragColor,vec4(tColor.rgb* (((vLightFront-emissive-(ambient * ambientLightColor)))+emissive+ambientLightColor), opacity), tColor.a);",
-								//"gl_FragColor =  mix(gl_FragColor,vec4(tColor.rgb* (((vLightFront-emissive-(ambient * ambientLightColor)) / diffuse)+emissive+ambientLightColor), opacity), tColor.a);",
-								"gl_FragColor =  mix(gl_FragColor,vec4(tColor.rgb * (vLightingOnly + ambientLightColor + emissive), opacity), tColor.a);",
-								
-								
-							"}",
-						
-					"}"
-		
-				].join("\n")		
 	};
 	
 	THREE.ShaderMaterial.call(this, parameters);
@@ -11146,9 +10762,9 @@ GIScene.Control.Walk = function ( object, domElement ) {
 		//only set if down and up coords are the same	
 		mouse.set(viewPortCoords.x, viewPortCoords.y, 1);
 		// var ray = projector.pickingRay(mouse, this.object); //-r76
-		var ray = raycaster.setFromCamera(mouse, this.object); //+r76
+		raycaster.setFromCamera(mouse, this.object); //+r76
 		var pickables = GIScene.Utils.getDescendants(this.scene.root).filter(function (e,i,a){return e.geometry;}); //r76 //@TODO currently all scene objects are pickable 
-		var pickResults = ray.intersectObjects(pickables);
+		var pickResults = raycaster.intersectObjects(pickables);
 		
 		var pickResult;
 		for(var i=0; i < pickResults.length; i++){
@@ -11664,6 +11280,7 @@ GIScene.Control.Pick = function(camera, config){
 											});
 	// var pickedPoint = new THREE.ParticleSystem(pickedPointGeom, pickedPointMaterial); //-r76
 	var pickedPoint = new THREE.Points(pickedPointGeom, pickedPointMaterial); //+r76
+	pickedPoint.renderOrder = -1;
 	pickedPoint.visible=false;
 	resultsLayer.root.add(pickedPoint);
 	
@@ -14317,7 +13934,16 @@ GIScene.Format = {
 	 * @static
 	 * @final 
 	 */
-	GeoJSON:6
+	GeoJSON:6,
+	/**
+	 * Specifies a text based format with 'x y z' values as lines to load simple point clouds. 
+	 * You can create .xyz files e.g. with MeshLab 
+	 * @property XYZ
+	 * @type Number
+	 * @static
+	 * @final 
+	 */
+	XYZ:6
 	
 };
 /**
@@ -16147,10 +15773,12 @@ GIScene.Grid.TileStore = function(config) {
 				// delete object.geometry;
 			// }
 			// var object = null;
+			if(object instanceof THREE.Object3D){ //while loading could also be a ModelLoader-Object
 			(object.material && object.material.isShared === false )? //isShared is defined e.g. in OverrideMaterialHandler/WMS.js
 				GIScene.Utils.disposeObject(object,true, true, true, true) :  
 				GIScene.Utils.disposeObject(object,true, false, false, true)
 				;
+			}
 			
 			delete this.store[oldestKey].object;
 			delete this.store[oldestKey].age;
@@ -16605,8 +16233,8 @@ GIScene.Layer.Grid = function(name, config){
 	this.load = []; //tiles to load
 	this.remove = []; //tiles to remove
 	
-	this.loading = new GIScene.Grid.TileStore(); //holds the xhr request while loading //new GIScene.Grid.IndexStore();
-	this.loaded  = new GIScene.Grid.TileStore(); //tiles already loaded
+	this.loading = new GIScene.Grid.TileStore({maxLength:null}); //holds the xhr request while loading //new GIScene.Grid.IndexStore();
+	this.loaded  = new GIScene.Grid.TileStore({maxLength:null}); //tiles already loaded
 	this.cache	 = new GIScene.Grid.TileStore({
 												maxLength:200
 												// deleteMaterials: !this.hasSharedOverrideMaterial ,
@@ -16827,7 +16455,7 @@ GIScene.Layer.Grid = function(name, config){
 				var requestUrl = this.config.service.getGetSceneUrl(gridIndex, this.grid);
 				
 				var loader = new GIScene.ModelLoader(); //need a loader for every parallel request
-				loader.load(requestUrl, this.format, onSuccess, null, onError);
+				loader.load(requestUrl, this.format, onSuccess, undefined, onError);
 				
 				this.loading.add(gridIndex, loader);
 				//?????mca3
@@ -16864,7 +16492,7 @@ GIScene.Layer.Grid = function(name, config){
 		// console.log("removeTile", gridIndex);
 		
 		// abort while still loading but not further needed
-		var xhrIsLoading = this.loading.getTile(gridIndex);
+		var xhrIsLoading = this.loading.getTile(gridIndex); //gridIndex contains ModelLoader-Object while loading
 		if(xhrIsLoading) {
 			/*console.log(xhrIsLoading);*/
 			xhrIsLoading.abort(); 
